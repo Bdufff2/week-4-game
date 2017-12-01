46f763b5f608bc571f8298b26c071fd914018169
duff.brian21@gmail.com <duff.brian21@gmail.com>
2017-11-30 20:08:59 -0800

    added basic game pseudo code in js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+
+// computer generates random number at the start of the game (19-120)
+// 4 crystals each assigned a random number at start of the game
+// when user clicks crystal should add to a total score
+// if user score less than computer gen number add selected crystal value to user score
+// if user score equals computer gen number add to wins
+// if user score exceeds computer gen number add to losses
